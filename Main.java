8bd7f083e354d8c9d0da43742f2339f0e70c6eb7
Albert <alberthovhannisyan24@gmail.com>
2023-02-20 18:35:27 +0400

    tnayin

--- Main.java
+++ Main.java
@@ -8,12 +8,8 @@ public class Main {
     public static final String URL = "C:\\Users\\Albert\\IdeaProjects\\lesson\\src\\data\\cucak.txt";
     public static void main(String[] args) throws Exception {
         System.out.println(Arrays.toString(FileService.read(URL)));
-        Student[] students = StudentService.createStudents(FileService.read(URL));
-        for (Student x: students) {
-            System.out.println(x);
-        }
-        StudentService.oldestStudent(students);
-        System.out.println();
-        System.out.println(StudentService.sortStudents(students));
+        Student[] students = StudentService.createStudents(FileService.read(URL), URL);
+        StudentService.newFile(students);
+
     }
 }
--- service/FileService.java
+++ service/FileService.java
@@ -2,9 +2,14 @@ package service;
 
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 
 public class FileService {
     public static String[] read(String url) throws Exception{
         return Files.readAllLines(Path.of(url)).toArray(new String[0]);
+    }
+    public static void write(String url, String text) throws Exception{
+        Files.write(Paths.get(url), text.getBytes(), StandardOpenOption.CREATE);
     }
 }
